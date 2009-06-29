ee43f21949160bed0ceccadd0924ef159aa1c837
Morgan Persson <lazydev@gmail.com>
2009-06-29 07:20:43 +0000

    oops...

--- src/Crap4n/MetricsBase.cs
+++ src/Crap4n/MetricsBase.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Crap4n
 {
     public abstract class MetricsBase
@@ -7,15 +5,5 @@ namespace Crap4n
         public string Class { get; set; }
         public string Method { get; set; }
         public string NameSpace { get; set; }
-    }
-
-    public interface IFoo
-    {
-        string Bar { get; set; }
-    }
-
-    public class Foo : IFoo
-    {
-        string IFoo.Bar { get; set; }        
     }
 }
