88201fb5c35b912adc0121843efb21dc0595ff79
Morgan Persson <lazydev@gmail.com>
2010-05-11 19:36:57 +0000

    fix build

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisibleAttribute(false)]
 [assembly: AssemblyVersionAttribute("0.2.0.0")]
 [assembly: AssemblyFileVersionAttribute("0.2.0.0")]
-[assembly: AssemblyCopyrightAttribute("Copyright © Morgan Persson 2009-2009")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Morgan Persson 2009-2010")]
 [assembly: AssemblyProductAttribute("Crap4n")]
 [assembly: AssemblyCompanyAttribute("Crap4n")]
 [assembly: AssemblyConfigurationAttribute("release")]
